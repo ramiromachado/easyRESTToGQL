136aba7498d3a4d65e4ad0784547dd0a480c26b4
Ramiro Machado <2271171+ramiromachado@users.noreply.github.com>
2020-05-23 04:45:22 -0300

    Tests designed

---
+++ test/unit/service.test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var should = require('chai').should();
+
+describe.skip('Service', function() {
+
+    describe('Success', function() {
+        it('should start the service for a simple entity with valid url', function() {
+        });
+
+        it('should start the service for two simple entity withoutConnection with valid url', function() {
+        });
+    });
+
+    describe('Error', function() {
+        it('should fail if fetching url for the entity is not given', function() {
+        });
+        it('should fail if the fields to map for the entity are not given', function() {
+        });
+        it('should fail if a field to map for the entity is of a not supported type', function() {
+        });
+    });
+});
